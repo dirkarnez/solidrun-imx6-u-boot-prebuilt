0bc3719761631d2ebc848661058112b4c5b198bd
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-02 20:45:29 +0800

    - update makeimage.sh

--- makeimage.sh
+++ makeimage.sh
@@ -14,14 +14,14 @@ set -e
 # export UBOOT_CONFIG="mx6cuboxi_defconfig"
 # make mrproper
 
-export PATH="/opt/gcc-linaro-6.5.0-2018.12-i686_arm-linux-gnueabihf/bin:$PATH" && \
-arm-linux-gnueabihf-gcc --version && \
+export PATH="/opt/arm-cortexa9_neon-linux-gnueabihf/bin:$PATH" && \
+arm-cortexa9_neon-linux-gnueabihf-gcc --version && \
 cd /workspace/u-boot-imx6 && \
-export CROSS_COMPILE="arm-linux-gnueabihf-" && \
+export CROSS_COMPILE="arm-cortexa9_neon-linux-gnueabihf-" && \
 announce "Building u-boot" && \
 make mx6cuboxi_defconfig && \
 echo "CONFIG_SPL_BOOT_DEVICE_SDHC=y" >> .config && \
-CROSS_COMPILE=arm-linux-gnueabihf- make -j8 && \
+make -j8 && \
 announce "image build appears to have been successful" && \
 announce "copying files" && \
 install -v -m644 -D ./SPL /dist/SPL && \
